f2407b46c9a292e18484676447b1a144e32717e0
Alper Tayfun <tayfun.alper@gmail.com>
2021-07-06 20:39:17 +0300

    test

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -7,7 +7,7 @@ const pools: PoolConfig[] = [
     stakingToken: tokens.starfield,
     earningToken: tokens.starfield,
     contractAddress: {
-      97: '0x0c873F865791869fD7F9067142c67AFb7Bd3D792',
+      97: '0xc7AEaA136657A9Ee1Bb21CE9a170F7E77151f46A',
       56: '0xFc4D8B845931cBBCa2FE545a874022ebAC1d0b8B',
     },
     poolCategory: PoolCategory.CORE,
